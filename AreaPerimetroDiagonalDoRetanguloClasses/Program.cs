3f3d2ec2e8e267880a241ff9330390d020e2de02
Victor <victorhugo_rosa@hotmail.com>
2019-10-26 10:09:39 -0300

    Calcular area, perimetro e diagonal de um retangulo

---
+++ AreaPerimetroDiagonalDoRetanguloClasses/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace AreaPerimetroDiagonalDoRetanguloClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo ret = new Retangulo();
+            
+
+            Console.Write("Entre com a Altura do Retangulo: ");
+            ret.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Entre com a largura do Retangulo: ");
+            ret.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("A area do retangulo é: "+ret.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine();
+            Console.Write("O Perimetro do retangulo é: "+ret.Perimetro().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine();
+            Console.Write("A diagonal do retangulo é: "+ret.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+            
+
+        }
+    }
+}
---
+++ AreaPerimetroDiagonalDoRetanguloClasses/Retangulo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AreaPerimetroDiagonalDoRetanguloClasses
+{
+    class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+        public double Perimetro()
+        {
+            return 2 * (Largura + Altura);
+        }
+        public double Diagonal()
+        {
+            double D = ((Largura * Largura) + (Altura * Altura));
+            return Math.Sqrt(D); 
+        }
+    }
+}
